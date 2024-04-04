78ce4b9bf0481369b0f0f7ee02cef51ca7959b3f
Mathis Houlet <houletmathis@gmail.com>
2024-04-04 18:17:50 +0200

    init next and wordpress headless

---
+++ cms-wordpress-app/next.config.js
@@ -0,0 +1,25 @@
+if (!URL.canParse(process.env.WORDPRESS_API_URL)) {
+  throw new Error(`
+    Please provide a valid WordPress instance URL.
+    Add to your environment variables WORDPRESS_API_URL.
+  `);
+}
+
+const { protocol, hostname, port, pathname } = new URL(
+  process.env.WORDPRESS_API_URL
+);
+
+/** @type {import('next').NextConfig} */
+module.exports = {
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "assets.vercel.com",
+        port: "",
+        pathname: "/image/upload/**",
+      },
+    ],
+  },
+};
